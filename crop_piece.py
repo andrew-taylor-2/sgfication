86b7fa66bac01fb87c47cdc50bf0f45a29851099
Andrew Taylor <andrewtaylor11235@gmail.com>
2024-03-14 00:07:11 -0400

    fills out crop_piece script, still untested tho

--- crop_piece.py
+++ crop_piece.py
@@ -13,19 +13,27 @@ def crop_piece(board_img, piece_img):
     if not circles:
         intersections, corners = imf.find_intersections(board_img, keep_intermediate=False, only_perpendicular=True)
 
-        #FILL REST HERE 
+        #for now we can consider interesctions and corners together
+        intersections.append(corners)
+
+        #if we have multiple, we can use one closest to image center as heuristic
+        img_center = (piece_img.shape[1] // 2, piece_img.shape[0] // 2)
+        centerest_intersection = min(intersections, key=lambda p: (p[0] - img_center[0])**2 + (p[1] - img_center[1])**2)
+
+        x, y = centerest_intersection
 
     else:
         #get biggest circle
-        circle=max(circles,key=lambda x: x[2])
+        circle=max(circles,key=lambda r: r[2])
         x, y, _ = circle
     
     #now crop it with bounding box centered on this spot
+    #   maxes and mins to ensure we don't go outside image boundaries
     
-    crop_left = x - column_spacing // 2
-    crop_right = crop_left + column_spacing
-    crop_top = y - row_spacing // 2
-    crop_bottom = crop_top + row_spacing
+    crop_left = max( 0 ,  x - column_spacing // 2 )
+    crop_right = min( crop_left + column_spacing , piece_img.shape[1] )
+    crop_top = max( 0 , y - row_spacing // 2 )
+    crop_bottom = min( crop_top + row_spacing , piece_img.shape[0] )
 
     # IF SCREENSHOT ISN"T BIG ENOUGH, RETURN ERROR THAT SCREENSHOT NEEDS TO BE BIGGER
 
