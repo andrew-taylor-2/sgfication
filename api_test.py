0fb7b4134054fc7b442767b34f3f3a043cfe6f99
Andrew Taylor <andrewtaylor11235@gmail.com>
2024-05-13 23:44:44 -0400

    fixes docker api test

--- api_test.py
+++ api_test.py
@@ -1,7 +1,26 @@
 import requests
+#
+# The URL of the FastAPI server endpoint
+url = 'http://127.0.0.1:5000/analyze/'
+#
+# Path to the image file you want to analyze
+image_path = r'C:\Users\andre\Software\Repos\sgfication\assets\board_shot.png'
+#
+# Open the image file in binary mode
+with open(image_path, 'rb') as image_file:
+    #
+    # Define the file payload as a dictionary
+    files = {'file': (image_path, image_file, 'image/png')}
+    # Send the POST request
+    response = requests.post(url, files=files)
+    # Print the response from the server
+    print(response.text)
 
-url = 'http://localhost:8000/upload'
-files = {'image': open('/path/to/your/image.jpg', 'rb')}
 
-response = requests.post(url, files=files)
-print(response.text)
+    
+# potential upload POST request?
+# url = 'http://localhost:8000/upload'
+# files = {'image': open('/path/to/your/image.jpg', 'rb')}
+
+# response = requests.post(url, files=files)
+# print(response.text)
