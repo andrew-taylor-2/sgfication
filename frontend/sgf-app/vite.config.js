b2a0161baab6c5282c0149ceed7d60d3d50e239f
Andrew Taylor <andrewtaylor11235@gmail.com>
2024-07-31 20:36:15 -0400

    needed to switch priority of preact/hooks and preact

--- frontend/sgf-app/vite.config.js
+++ frontend/sgf-app/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      'preact/hooks': 'react',
+      preact: 'react',
       'pdfjs-dist': path.resolve(__dirname, 'node_modules/pdfjs-dist')
-    }
+    },
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
   }
-}); // I"m NOT aliasing preact to react like in the sabaki install instructions -- i this causes issues down the line then change it back
+});
