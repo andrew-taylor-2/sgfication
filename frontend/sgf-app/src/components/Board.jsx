f5511d30d30cbd1a21f6b238200cbbca6088b00c
Andrew Taylor <andrewtaylor11235@gmail.com>
2024-08-05 13:47:04 -0400

    adds default board state

--- frontend/sgf-app/src/components/Board.jsx
+++ frontend/sgf-app/src/components/Board.jsx
@@ -4,40 +4,48 @@ import '@sabaki/shudan/css/goban.css';
 import sgf from '@sabaki/sgf'; // Import the SGF parser
 
 const Board = ({ sgfData }) => {
-  const [signMap, setSignMap] = useState([]);
+
+  // let's make a default
+  const defaultBoardSize = 19;
+  const defaultSignMap = Array.from({ length: defaultBoardSize}, () => Array(defaultBoardSize).fill(0));
+
+  const [signMap, setSignMap] = useState(defaultSignMap);
+
   useEffect(() => {
-    const parseSGF = (sgfData) => {
-      const collection = sgf.parse(sgfData);
-      const gameTree = collection[0];
-      const rootNode = gameTree.data;
-      const boardSize = parseInt(rootNode.SZ[0]);
-      const signMap = Array.from({ length: boardSize }, () => Array(boardSize).fill(0)); // Initialize a 2D array with 0 (empty)
-
-      const parseCoordinates = (coords) => {
-        return coords.map(coord => {
-          const x = coord.charCodeAt(0) - 97;
-          const y = coord.charCodeAt(1) - 97;
-          return [x, y];
-        });
+    if (sgfData) {
+      const parseSGF = (sgfData) => {
+        const collection = sgf.parse(sgfData);
+        const gameTree = collection[0];
+        const rootNode = gameTree.data;
+        const boardSize = parseInt(rootNode.SZ[0]);
+        const signMap = Array.from({ length: boardSize }, () => Array(boardSize).fill(0)); // Initialize a 2D array with 0 (empty)
+  
+        const parseCoordinates = (coords) => {
+          return coords.map(coord => {
+            const x = coord.charCodeAt(0) - 97;
+            const y = coord.charCodeAt(1) - 97;
+            return [x, y];
+          });
+        };
+  
+        if (rootNode.AB) {
+          parseCoordinates(rootNode.AB).forEach(([x, y]) => {
+            signMap[y][x] = 1; // Black stones
+          });
+        }
+  
+        if (rootNode.AW) {
+          parseCoordinates(rootNode.AW).forEach(([x, y]) => {
+            signMap[y][x] = -1; // White stones
+          });
+        }
+  
+        return signMap;
       };
-
-      if (rootNode.AB) {
-        parseCoordinates(rootNode.AB).forEach(([x, y]) => {
-          signMap[y][x] = 1; // Black stones
-        });
-      }
-
-      if (rootNode.AW) {
-        parseCoordinates(rootNode.AW).forEach(([x, y]) => {
-          signMap[y][x] = -1; // White stones
-        });
-      }
-
-      return signMap;
-    };
-
-    const newSignMap = parseSGF(sgfData);
-    setSignMap(newSignMap);
+  
+      const newSignMap = parseSGF(sgfData);
+      setSignMap(newSignMap);
+  }
   }, [sgfData]);
 
   return (
